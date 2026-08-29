1e0a632c821744eb7d1d96896f7444f57dce805a
peonyking <peonykings@gmail.com>
2026-08-29 18:05:19 +0800

    Delete 滤镜特效.js

--- 滤镜特效.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    var s = document.createElement('style');
-    s.textContent = ".img-wrap, #v img, #lv img { filter: contrast(1.2) saturate(1.1); transition: filter 0.5s ease; }";
-    document.head.appendChild(s);
-})();
